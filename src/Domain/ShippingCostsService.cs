0ccd0a526d070baf1ad86b66c353da9bdbd6a28f
João Rosa <joaoasrosa@gmail.com>
2019-01-17 16:12:03 +0100

    Fix bug

--- src/Domain/ShippingCostsService.cs
+++ src/Domain/ShippingCostsService.cs
@@ -2,7 +2,7 @@ namespace Domain
 {
     public class ShippingCostsService
     {
-        private const decimal PricePerGram = 0.02m;
+        private const decimal PricePerGram = 0.002m;
 
         public ShippingCosts CalculateShippingCosts(Parcel parcel)
         {
