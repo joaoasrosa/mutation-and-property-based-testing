1d9c2ad7786a6229913449599423c281740222f0
João Rosa <joaoasrosa@gmail.com>
2019-01-17 15:28:38 +0100

    Add test shell

---
+++ tests/Domain.Tests.Unit/WhenCalculatingShippingCosts.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Domain.Tests.Unit
+{
+    public class WhenCalculatingShippingCosts
+    {
+        [Fact]
+        public void GivenParcelWithSumOfItemsIsOneKilo_ThenShippingPriceIsTwoEuro()
+        {
+        }
+    }
+}
